b459c760ed4cbb853b5f5855c5a163db2b0b9773
Tanisha borana <tanishaborana05@gmail.com>
2024-02-24 16:02:29 +0530

    Episode

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 // const heading = React.createElement("h1", {id: "heading"}, "Hello World from React"); 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 // console.log(heading);
 // root.render(heading);
 const parent = React.createElement("div",{id: "parent"},
 React.createElement("div",{id:"child"},
-React.createElement("h1",{}, "I am h1 tag")
+React.createElement("h1",{}, "I am hhh tag")
 )
 );
 console.log(parent);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #  NAMASTE REACT 
+
+# Parcel 
+-Dev Build
+-Local Server
+- HMR- Hot Module Replacement
+-File Replacement Algorith- written in C++
