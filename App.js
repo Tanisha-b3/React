c5084b6b550a250fcd196d340ab0df8619cf5d67
Tanisha borana <tanishaborana05@gmail.com>
2024-02-26 13:16:23 +0530

    Episode

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React"); 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// console.log(heading);
-// root.render(heading);
-const parent = React.createElement("div",{id: "parent"},
-React.createElement("div",{id:"child"},
-React.createElement("h1",{}, "I am hhh tag")
-)
+const heading = React.createElement(
+    "h1",{id: "heading"},
+    "Namaste React"
 );
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const heading1 = React.createElement(
+    "h1",{id: "heading1"},
+    "Namaste React"
+);
+const root = ReactDOM.createRoot(
+    document.getElementById("root"));
+    root.render(heading);
